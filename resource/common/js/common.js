ad2ab03edfabb379e214719b6dde58736be9ef90
MomoKiller <1226421458@qq.com>
2018-09-04 14:58:47 +0800

    提交代码

--- resource/common/js/common.js
+++ resource/common/js/common.js
@@ -13,25 +13,8 @@ module_key = {
 };
 
 /**
- * 公共事件
- */
-$('.common_head .login').bind('click', function () {
-    location.href = 'login.html';
-});
-$('.common_head .register').bind('click', function () {
-    location.href = 'login.html?register=true';
-});
-$('.common_head .contact').bind('click', function () {
-
-});
-$('.common_head .user_info').bind('click', function () {
-
-});
-
-/**
  * 表单验证
  */
-
 
 /**
  * cookie
@@ -76,3 +59,43 @@ function getQueryString(name) {
     var result = window.location.search.substr(1).match(reg);
     return result ? decodeURIComponent(result[2]) : null;
 };
+
+
+
+/**
+ * 头尾 
+ */
+function loadHead() {
+    var headUrl = './resource/common/html/head.html'
+    $.get(headUrl, {}, function (head) {
+        $('.common_head').append(head);
+        $('.common_head .home').bind('click', function () {
+            location.href = 'index.html';
+        });
+        $('.common_head .login').bind('click', function () {
+            location.href = 'login.html';
+        });
+        $('.common_head .register').bind('click', function () {
+            location.href = 'login.html?register=true';
+        });
+        $('.common_head .contact').bind('click', function () {
+    
+        });
+        $('.common_head .user_info').bind('click', function () {
+    
+        });
+    });  
+}
+function loadTail() {
+    var tailUrl = './resource/common/html/tail.html';
+    $.get(tailUrl, {}, function (tail) {
+        $('.common_tail').append(tail);
+    });
+}
+
+
+// 初始化
+$(function () {
+    loadHead();
+    loadTail();
+})
